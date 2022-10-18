a08864f1e6e329d6963e7f1e7e32146ba9b8689f
justcoddev <edisonpaulcrz@gmail.com>
2022-10-17 23:29:55 -0500

    Property Binding/Enlace de propiedad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Teddy'
+  textoPlaceholder = 'Escribir aquí'
+  deshabilitado = true;
+  imgSrc = 'https://angular.io/assets/images/logos/angular/angular.png'
   constructor() {
-    setInterval(() => this.nombre = "Pool", 3000);
+    setInterval(() => this.nombre = 'Pool', 3000);
+    setInterval(() => this.deshabilitado = false, 3000);
   }
   getSuma(numero1: number, numero2: number) {
     return numero1 + numero2
