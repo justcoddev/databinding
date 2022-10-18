7b7874b5671b5afd726993337222c3072d50a2f6
justcoddev <edisonpaulcrz@gmail.com>
2022-10-17 23:54:08 -0500

    EVENT BINDING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // INTERPOLACIÓN
   nombre = 'Teddy'
+
+  // PROPERTY BINDING
   textoPlaceholder = 'Escribir aquí'
   deshabilitado = true;
   imgSrc = 'https://angular.io/assets/images/logos/angular/angular.png'
+
   constructor() {
-    setInterval(() => this.nombre = 'Pool', 3000);
-    setInterval(() => this.deshabilitado = false, 3000);
+    setInterval(() => this.nombre = 'Pool', 3000); //INTERPOLACION
+    setInterval(() => this.deshabilitado = false, 3000); //PROPERTY BINDING
   }
+
+  // FUNCION PARA INTERPOLACION
   getSuma(numero1: number, numero2: number) {
     return numero1 + numero2
+  }
+
+  // EVENT BINDING
+  texto = 'Esto es un video sobre event binding'
+  cambiarTexto(): void {
+    this.texto = "En el proximo video vamos a ver Two Way Data-Binding"
   }
 }
